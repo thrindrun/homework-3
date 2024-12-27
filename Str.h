a4c89fb9f92ecffa7a3b637d420375e6729cb428
thrindrun <burakbulut801@gmail.com>
2024-12-27 22:06:25 +0300

    Update Str.h

--- Str.h
+++ Str.h
@@ -1,1 +1,14 @@
-
+class Str {
+    public:
+        Str();
+        Str(const char *arr);
+        Str(const Str& src);
+        char* getString();
+        int getSize();
+        void print();
+        void reverse();
+        void uppercase();
+    private:
+        int Size;
+        char* str;
+};
