98ea488b711aeccee3c0ec85a5a774648c9e81cc
thrindrun <burakbulut801@gmail.com>
2024-12-27 12:15:20 +0300

    Create test_Str.cpp

---
+++ test_Str.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Str.h"
+using namespace std;
+
+int main(){
+    Str s1;
+    char courseName[] = "cpp";
+    Str s2(courseName);
+    Str s3(s2);
+
+    s2.print(); //cpp
+    s3.print(); //cpp
+
+    s1=s2; 
+    s1.print(); //cpp
+    ++s1;
+    s1.print(); //dqq
+    s1[0]='P'; //Pqq
+
+    if (s1==s2)
+        cout << "these two objects are the same" << endl;
+    else
+        cout << "these two objects are not the same" << endl;
+    // these two objects are not the same
+
+    cout << s1.getSize() << endl; //4 (including the null)
+    s2.reverse();
+    s2.print(); //ppc
+    s2.uppercase();
+    s2.print(); //PPC
+
+    return 0;
+}
