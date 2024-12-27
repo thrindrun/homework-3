9fc821033017d78e93503c8752b05d71548b1b78
thrindrun <burakbulut801@gmail.com>
2024-12-28 02:32:25 +0300

    Update point2d.h

---
+++ q2/point2d.h
@@ -0,0 +1,11 @@
+class point2D {
+    private:
+        double x;
+        double y;
+    public:
+        point2D(double a,double b);
+        void setX();
+        double getX();
+        void setY();
+        double getY();
+};
