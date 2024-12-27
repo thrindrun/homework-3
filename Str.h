f1cef4f26e12c221d853d944d7aaa5e08c45f737
thrindrun <burakbulut801@gmail.com>
2024-12-28 00:57:51 +0300

    Update Str.h

--- Str.h
+++ Str.h
@@ -1,14 +1,25 @@
+#ifndef STR.H
+#define STR.H
+
 class Str {
     public:
         Str();
         Str(const char *arr);
         Str(const Str& src);
-        char* getString();
-        int getSize();
-        void print();
+        Str& operator=(const Str& src);
+        Str& operator++();
+        char& operator[](int index);
+        bool operator==(const Str& src);
+        bool operator!=(const Str& src);
+        char* getString() const;
+        int getSize() const;
+        void print() const;
         void reverse();
         void uppercase();
+        ~Str();
     private:
         int Size;
         char* str;
 };
+
+#endif
