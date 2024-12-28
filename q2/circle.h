db89f42a21e558c9b9c39b218c16d1214de988a6
thrindrun <burakbulut801@gmail.com>
2024-12-28 23:18:17 +0300

    little fix

--- q2/circle.h
+++ q2/circle.h
@@ -1,7 +1,7 @@
 #include "point2d.h"
 class Circle {
     public:
-        Circle(const double x,const double y,const double r);
+        Circle(const double x=0,const double y=0,const double r=1);
         Circle(const Point2D& src,const double r);
         void setRadius(const double r);
         void setCenter(const double x,const double y);
